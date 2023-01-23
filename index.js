f3936ec677a9b744ef209ce9af059ec10a85d00c
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-01-23 15:07:27 +0600

    js закрепление

---
+++ index.js
@@ -0,0 +1,69 @@
+let myName = "Atak";
+let myLastName = "Oskonbaev";
+let myTown = "Karakol";
+let myCountry = "Kyrgyzstan";
+let favoriteGame = "Football";
+let myLaptop = "Lenovo";
+let nativeLanguage = "Russian";
+let nativeLanguage2 = "Kyrgyz";
+let foreignLanguage = "English";
+let favNumber = "seven";
+
+myName = "Lol";
+
+let height = 180;
+let currentYear = 2023;
+let date = 23;
+let favoriteNumber = 7;
+let myAge = 17;
+
+let favRap = true;
+let favPop = false;
+
+let num = 5 > 3;
+let num2 = 4 < 3;
+let num3 = 4 === 4;
+let num4 = 3 !== 6;
+let num5 = 6 >= 2;
+
+if (5 > 3) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
+
+if (4 < 3) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
+
+if (4 === 4) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
+
+if (5 !== 4) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
+
+let a = 0;
+if (a > 0) {
+  console.log(a + " is positive");
+}
+else if (a < 0) {
+  console.log(a + " is negative");
+}
+else if (a === 0) {
+  console.log(a + " is zero");
+}
+else {
+  console.log(a + " is not a number");
+}
